11bd24cde4d6d34a74123d5a7ae7d209b5cc1915
Giuseppe93k <164879856+Giuseppe93k@users.noreply.github.com>
2024-05-09 09:36:32 +0100

    Basic JavaScript Project Assignment
    Here I just did a simple assignment consisting of:
    using the "document.write(), window.alert(), assigning variables, concatenating strings  and writing exspressions

---
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,19 @@
+// This is my window.alert()
+window.alert("Hello, World");
+
+// This is my document.write()
+document.write("This is my Basic JavaScript project Assignment");
+
+// creating variable 1 of 2 
+var Sent1 = "This is the beginning of the string"
+
+// creating variable 2 of 2 
+var Sent2 = " and this is the end of the string"
+
+//concatenate Sent1 & Sent2 
+document.write(Sent1 + Sent2);
+
+// writing an expression
+document.write(20-10);
+
+
